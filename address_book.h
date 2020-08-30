92679f82733c3a4b8d7743572c33a49e76d18739
“sandip5” <singhsandeep.singh97@gmail.com>
2020-08-30 19:36:25 +0530

    ADDED- Ability To Add New Person And Display It

--- address_book.h
+++ address_book.h
@@ -12,6 +12,7 @@ namespace address_book_spc
         void add_Person();
         void set_Person_Details_In_Book(std::vector<person_spc::person> &person_Contact_Book);
         std::vector<person_spc::person> get_Person_Details_From_Book();
+        void display_Person_Details();
     };
 
     void address_book::set_Person_Details_In_Book(std::vector<person_spc::person> &person_Contact_Book)
@@ -34,6 +35,12 @@ namespace address_book_spc
 
         for(int person_Counter = 0; person_Counter < number_Of_Person; person_Counter++)
         {
+            if(person_Counter > 0)
+            {
+                std::cout << "Enter Next Person Details: " << std::endl;
+            }
+
+            std::cin.ignore(1,'\n');
             std::cout << "Enter Full Name Of Person: ";
             std::string full_Name;
             std::getline (std::cin, full_Name);
@@ -59,5 +66,19 @@ namespace address_book_spc
         }
 
         address_book::set_Person_Details_In_Book(person_Contact_Book);
+    }
+
+    void address_book::display_Person_Details()
+    {
+        for(person_spc::person display : person_Contact_Book)
+        {
+            std::cout << "*---------------Address Book All Person Details---------------------*" << std::endl;
+            std::cout << "Person Full Name: " << display.get_Full_Name() << std::endl;
+            std::cout << "Person Address: " << display.get_Address() << std::endl;
+            std::cout << "Person City: " << display.get_City() << std::endl;
+            std::cout << "Person State: " << display.get_State() << std::endl;
+            std::cout << "Person Zip: " << display.get_Zip() << std::endl;
+            std::cout << "Person Phone Number: " << display.get_Phone_Number() << std::endl;
+        }
     }
 }
